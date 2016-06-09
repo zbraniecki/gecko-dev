2bd6d770ffa89ca7a426e728d46166155de0dcb4
Staś Małolepszy <stas@mozilla.com>
2016-06-09 13:09:36 +0200

    Group elements by localization
    Take full advantage of formatEntities which takes an array of key by gruping
    translatable elements by their Localization object.

--- toolkit/content/l20n-content-html.js
+++ toolkit/content/l20n-content-html.js
@@ -1736,6 +1736,34 @@ class LocalizationObserver extends Map {
     );
   }
 
+  handleEvent() {
+    return this.requestLanguages();
+  }
+
+  requestLanguages(requestedLangs) {
+    const localizations = Array.from(this.values());
+    return Promise.all(
+      localizations.map(l10n => l10n.requestLanguages(requestedLangs))
+    ).then(
+      () => this.translateAllRoots()
+    )
+  }
+
+  setAttributes(element, id, args) {
+    element.setAttribute('data-l10n-id', id);
+    if (args) {
+      element.setAttribute('data-l10n-args', JSON.stringify(args));
+    }
+    return element;
+  }
+
+  getAttributes(element) {
+    return {
+      id: element.getAttribute('data-l10n-id'),
+      args: JSON.parse(element.getAttribute('data-l10n-args'))
+    };
+  }
+
   observeRoot(root, l10n = this.get('main')) {
     this.localizationsByRoot.set(root, l10n);
     if (!this.rootsByLocalization.has(l10n)) {
@@ -1773,93 +1801,6 @@ class LocalizationObserver extends Map {
         }
       }
     }
-  }
-
-  requestLanguages(requestedLangs) {
-    const localizations = Array.from(this.values());
-    return Promise.all(
-      localizations.map(l10n => l10n.requestLanguages(requestedLangs))
-    ).then(
-      () => this.translateAllRoots()
-    )
-  }
-
-  handleEvent() {
-    return this.requestLanguages();
-  }
-
-  getTranslatables(element) {
-    const nodes = Array.from(element.querySelectorAll('[data-l10n-id]'));
-
-    if (typeof element.hasAttribute === 'function' &&
-        element.hasAttribute('data-l10n-id')) {
-      nodes.push(element);
-    }
-
-    return nodes;
-  }
-
-  translateMutations(mutations) {
-    const targets = new Set();
-
-    for (let mutation of mutations) {
-      switch (mutation.type) {
-        case 'attributes':
-          targets.add(mutation.target);
-          break;
-        case 'childList':
-          for (let addedNode of mutation.addedNodes) {
-            if (addedNode.nodeType === addedNode.ELEMENT_NODE) {
-              if (addedNode.childElementCount) {
-                this.getTranslatables(addedNode).forEach(
-                  targets.add.bind(targets)
-                );
-              } else {
-                if (addedNode.hasAttribute('data-l10n-id')) {
-                  targets.add(addedNode);
-                }
-              }
-            }
-          }
-          break;
-      }
-    }
-
-    if (targets.size === 0) {
-      return;
-    }
-
-    this.translateElements(Array.from(targets));
-  }
-
-  getLocalizationForElement(elem) {
-    return this.get(elem.getAttribute('data-l10n-bundle') || 'main');
-  }
-
-  // XXX the following needs to be optimized, perhaps getTranslatables should 
-  // sort elems by localization they refer to so that it is easy to group them, 
-  // handle each group individually and finally concatenate the resulting 
-  // translations into a flat array whose elements correspond one-to-one to 
-  // elems?
-  getElementsTranslation(elems) {
-    const keys = elems.map(elem => {
-      const args = elem.getAttribute('data-l10n-args');
-      return [
-        this.getLocalizationForElement(elem),
-        elem.getAttribute('data-l10n-id'),
-        args ?
-          JSON.parse(args.replace(reHtml, match => htmlEntities[match])) :
-          null
-      ];
-    });
-
-    return Promise.all(
-      keys.map(
-        ([l10n, id, args]) => l10n.formatEntities([[id, args]]).then(
-          translations => [l10n, translations]
-        )
-      )
-    );
   }
 
   translateAllRoots() {
@@ -1908,37 +1849,108 @@ class LocalizationObserver extends Map {
     });
   }
 
+  translateMutations(mutations) {
+    const targets = new Set();
+
+    for (let mutation of mutations) {
+      switch (mutation.type) {
+        case 'attributes':
+          targets.add(mutation.target);
+          break;
+        case 'childList':
+          for (let addedNode of mutation.addedNodes) {
+            if (addedNode.nodeType === addedNode.ELEMENT_NODE) {
+              if (addedNode.childElementCount) {
+                this.getTranslatables(addedNode).forEach(
+                  targets.add.bind(targets)
+                );
+              } else {
+                if (addedNode.hasAttribute('data-l10n-id')) {
+                  targets.add(addedNode);
+                }
+              }
+            }
+          }
+          break;
+      }
+    }
+
+    if (targets.size === 0) {
+      return;
+    }
+
+    this.translateElements(Array.from(targets));
+  }
+
   translateFragment(frag) {
     return this.translateElements(this.getTranslatables(frag));
   }
 
   translateElements(elements) {
-    return this.getElementsTranslation(elements).then(
-      translations => this.applyTranslations(elements, translations));
+    const elemsByL10n = this.groupElementsByLocalization(elements);
+    return this.getElementsTranslation(elemsByL10n).then(
+      translations => this.applyTranslations(elemsByL10n, translations)
+    );
   }
 
-  applyTranslations(elems, translations) {
+  applyTranslations(elemsByL10n, translationsByL10n) {
     this.pause();
-    for (let i = 0; i < elems.length; i++) {
-      const [l10n, [translation]] = translations[i];
-      l10n.overlayElement(elems[i], translation);
+    for (let [l10n, elems] of elemsByL10n) {
+      const translations = translationsByL10n.get(l10n);
+      for (let i = 0; i < elems.length; i++) {
+        l10n.overlayElement(elems[i], translations[i]);
+      }
     }
     this.resume();
   }
 
-  setAttributes(element, id, args) {
-    element.setAttribute('data-l10n-id', id);
-    if (args) {
-      element.setAttribute('data-l10n-args', JSON.stringify(args));
-    }
-    return element;
+  groupElementsByLocalization(elements) {
+    return Array.from(elements).reduce(
+      (seq, elem) => {
+        const l10n = this.getLocalizationForElement(elem);
+        const group = (seq.get(l10n) || []).concat(elem);
+        return seq.set(l10n, group);
+      }, new Map()
+    );
   }
 
-  getAttributes(element) {
-    return {
-      id: element.getAttribute('data-l10n-id'),
-      args: JSON.parse(element.getAttribute('data-l10n-args'))
-    };
+  getTranslatables(element) {
+    const nodes = Array.from(element.querySelectorAll('[data-l10n-id]'));
+
+    if (typeof element.hasAttribute === 'function' &&
+        element.hasAttribute('data-l10n-id')) {
+      nodes.push(element);
+    }
+
+    return nodes;
+  }
+
+  getLocalizationForElement(elem) {
+    return this.get(elem.getAttribute('data-l10n-bundle') || 'main');
+  }
+
+  getKeysForElements(elems) {
+    return elems.map(elem => {
+      const id = elem.getAttribute('data-l10n-id');
+      const args = elem.getAttribute('data-l10n-args');
+
+      return args ?
+        [id, JSON.parse(args.replace(reHtml, match => htmlEntities[match]))] :
+        id;
+    });
+  }
+
+  getElementsTranslation(elemsByL10n) {
+    return Promise.all(
+      Array.from(elemsByL10n).map(
+        ([l10n, elems]) => l10n.formatEntities(this.getKeysForElements(elems))
+      )
+    ).then(
+      translationsList => Array.from(elemsByL10n.keys()).reduce(
+        (seq, cur, idx) => seq.set(cur, translationsList[idx]),
+        new Map()
+      )
+    );
   }
 
 }
